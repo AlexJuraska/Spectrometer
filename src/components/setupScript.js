ac29e22aedf290603a391d0ea875ef40a34a9cb8
AlexJuraska <151767106+AlexJuraska@users.noreply.github.com>
2025-06-01 23:29:56 +0200

    Moved calibration into the main recoding screen

--- src/components/setupScript.js
+++ src/components/setupScript.js
@@ -1,22 +1,17 @@
 
-let currentStep = 2;
+let calibrationScreen = false;
 
-/**
- * Changes the step in the stepper
- * @param direction -1 = previous, 1 = next
- */
-function changeStep(direction) {
+function changeCalibrationScreen() {
 
-    document.getElementById(`step${currentStep}`).classList.remove('active');   //Remove the previous step
-    currentStep += direction;   // Add the following step
-    document.getElementById(`step${currentStep}`).classList.add('active');  //Add the current step
-
-    if (currentStep === 2) {
-        document.getElementById('prevButton').disabled = false;
-        document.getElementById('nextButton').disabled = true;
-        drawGridCalibration();
-    } else if (currentStep === 3) {
-        document.getElementById('stepper-buttons').classList.add('disabled');
-        getCameras();
+    if (calibrationScreen) {
+        document.getElementById('graphWindowContainer').style.display = 'block';
+        document.getElementById('calibrationWindowContainer').style.display = 'none';
+        document.getElementById('calibrationSettings').style.display = 'none';
+    } else {
+        document.getElementById('graphWindowContainer').style.display = 'none';
+        document.getElementById('calibrationWindowContainer').style.display = 'block';
+        document.getElementById('calibrationSettings').style.display = 'block';
     }
+
+    calibrationScreen = !calibrationScreen;
 }

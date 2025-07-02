232a2d93e93cdfbf1adbae13e45b666f63bf2d64
AlexJuraska <151767106+AlexJuraska@users.noreply.github.com>
2025-07-02 20:14:56 +0200

    IT FECKIN SLIDES

--- src/components/setupScript.js
+++ src/components/setupScript.js
@@ -1,4 +1,6 @@
-
+/**
+ * Changes the active settings screen on the sidebar
+ */
 function changeSettingsScreen(changeTo) {
 
     if (changeTo === "Graph") {
@@ -14,6 +16,10 @@ function changeSettingsScreen(changeTo) {
         document.getElementById('changeFromCalibrationButton').style.display = 'none';
         document.getElementById('changeToLongExpoButton').style.display = 'block';
         document.getElementById('changeFromLongExpoButton').style.display = 'none';
+
+        changeDisplayScreen('main');
+        changeDisplayScreen('settings');
+        changeDisplayScreen("graph");
     } else if (changeTo === "Calibration") {
         document.getElementById('graphWindowContainer').style.display = 'none';
         document.getElementById('calibrationWindowContainer').style.display = 'block';
@@ -27,6 +33,9 @@ function changeSettingsScreen(changeTo) {
         document.getElementById('changeFromCalibrationButton').style.display = 'block';
         document.getElementById('changeToLongExpoButton').style.display = 'block';
         document.getElementById('changeFromLongExpoButton').style.display = 'none';
+
+        changeDisplayScreen('main');
+        changeDisplayScreen('settings');
 
         resizeCanvasToDisplaySize(graphCtxCalibration, graphCanvasCalibration, "Calibration");
     } else if (changeTo === "LongExpo") {
@@ -43,7 +52,41 @@ function changeSettingsScreen(changeTo) {
         document.getElementById('changeToLongExpoButton').style.display = 'none';
         document.getElementById('changeFromLongExpoButton').style.display = 'block';
 
+        changeDisplayScreen('main');
+        changeDisplayScreen('settings');
+        changeDisplayScreen("graph");
     }
+}
+
+/**
+ * Changes which sections are displayed on the main screen
+ */
+function changeDisplayScreen(action) {
+    const settings = document.getElementById('sidebar');
+    const drawer = document.getElementById('graphSettingsDrawer');
+    const handle = document.getElementById('sidebarToggleHandle');
+    const detectionArea = document.getElementById('sidebarHoverZone');
+
+    if (action === "main") {
+        settings.classList.add('hidden');
+        drawer.classList.add('hidden');
+
+        handle.classList.remove('moved');
+        detectionArea.classList.remove('moved');
+
+        document.getElementById("graphCanvas").classList.remove("withDrawer");
+    } else if (action === "settings") {
+        const isHidden = settings.classList.toggle('hidden');
+        handle.innerHTML = isHidden ? '↪' : '↩';
+
+        handle.classList.toggle('moved');
+        detectionArea.classList.toggle('moved');
+    } else if (action === "graph") {
+        document.getElementById("graphCanvas").classList.toggle("withDrawer");
+        drawer.classList.toggle('hidden');
+    }
+
+    document.activeElement.blur();
 }
 
 function callError(errorMessageTranslate) {
@@ -126,3 +169,26 @@ function  closeCameraRecordingWindow(){
     const blocker = document.getElementById("infoPopupBlock");
     blocker.classList.remove('show');
 }
+
+const handle = document.getElementById('sidebarToggleHandle');
+const hoverZone = document.getElementById('sidebarHoverZone');
+
+hoverZone.addEventListener('mouseenter', () => {
+    handle.style.opacity = '1';
+    handle.style.pointerEvents = 'auto';
+});
+
+handle.addEventListener('mouseenter', () => {
+    handle.style.opacity = '1';
+    handle.style.pointerEvents = 'auto';
+});
+
+hoverZone.addEventListener('mouseleave', () => {
+    handle.style.opacity = '0';
+    handle.style.pointerEvents = 'none';
+});
+
+handle.addEventListener('mouseleave', () => {
+    handle.style.opacity = '0';
+    handle.style.pointerEvents = 'none';
+});

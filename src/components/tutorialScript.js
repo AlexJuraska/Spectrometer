369ed44642154af34573e0f2f95a7a5056839014
AlexJuraska <151767106+AlexJuraska@users.noreply.github.com>
2025-08-20 23:40:18 +0200

    Delete me

--- src/components/tutorialScript.js
+++ src/components/tutorialScript.js
@@ -15,28 +15,3 @@ function showSection(name) {
         }
     }
 }
-
-// const sidebar = document.getElementById("sidebar-tutorial");
-// const hoverZone = document.getElementById("sidebarHoverZone");
-//
-// // Track if the sidebar is currently shown
-// let isSidebarVisible = false;
-//
-// // Move sidebar to mouse Y only when hovering over the zone
-// hoverZone.addEventListener("mousemove", (e) => {
-//     if (!isSidebarVisible) {
-//         sidebar.style.top = e.clientY + "px";
-//     }
-// });
-//
-// // Show sidebar when entering the zone
-// hoverZone.addEventListener("mouseenter", () => {
-//     sidebar.classList.remove('hidden');
-//     isSidebarVisible = true;
-// });
-//
-// // Hide sidebar when leaving the zone
-// hoverZone.addEventListener("mouseleave", () => {
-//     sidebar.classList.add('hidden');
-//     isSidebarVisible = false;
-// });

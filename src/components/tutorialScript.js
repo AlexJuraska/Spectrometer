02840fa0d654b25a1315ddb811e5901f3e1c34e8
BigBoiBombom <kosina2@uniba.sk>
2025-09-08 00:34:06 +0200

    Start with how to page

--- src/components/tutorialScript.js
+++ src/components/tutorialScript.js
@@ -1,5 +1,5 @@
 
-let sections = ["Section1", "Section2"]
+let sections = ["Section1", "Section2", "Section3", "Section4", "Section5", "Section6", "Section7", "Section8", "Section9", "Section10"];
 
 function showSection(name) {
     if (!sections.includes(name)) {
